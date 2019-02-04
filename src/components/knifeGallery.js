c0817288ab2a95dcd5b9408042c23fc6445bc13a
Will Adams <bushblade@gmail.com>
2019-02-04 21:37:42 +0000

    custom image component

--- src/components/knifeGallery.js
+++ src/components/knifeGallery.js
@@ -3,6 +3,25 @@ import axios from 'axios'
 import Gallery from 'react-photo-gallery'
 import Lightbox from 'react-images'
 import { options } from '../config'
+import styled from 'styled-components'
+
+const Img = styled.img`
+  box-shadow: -1px 3px 6px 1px rgba(0, 0, 0, 0.3);
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  :hover {
+    transform: scale(1.01) translate3d(0, -2px, 0);
+    box-shadow: -2px 5px 8px 2px rgba(0, 0, 0, 0.3);
+  }
+`
+
+const ImageComponent = ({ index, onClick, photo, margin }) => (
+  <Img
+    {...photo}
+    style={{ margin, height: photo.height, width: photo.width }}
+    onClick={e => onClick(e, { index, photo })}
+  />
+)
 
 const KnifeGallery = ({ album }) => {
   const [images, setImages] = useState([])
@@ -37,6 +56,8 @@ const KnifeGallery = ({ album }) => {
             setCurrent(obj.index)
             setOpen(true)
           }}
+          ImageComponent={ImageComponent}
+          margin={5}
         />
       )}
       <Lightbox
