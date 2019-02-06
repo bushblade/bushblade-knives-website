b2cf89b685a6b62fccd0359914cc5064f905d8dc
Will Adams <bushblade@gmail.com>
2019-02-06 09:59:21 +0000

    hover effect

--- src/components/knifeGallery.js
+++ src/components/knifeGallery.js
@@ -8,10 +8,17 @@ const ImageWrapper = styled.div`
   box-shadow: -1px 3px 6px 1px rgba(0, 0, 0, 0.3);
   transition: all 0.2s ease-in-out;
   border-radius: 2px;
+  overflow: hidden;
   cursor: pointer;
+  div {
+    transition: transform 2s;
+  }
   :hover {
-    transform: scale(1.01) translate3d(0, -2px, 0);
+    /* transform: scale(1.01) translate3d(0, -2px, 0); */
     box-shadow: -2px 5px 8px 2px rgba(0, 0, 0, 0.3);
+    div {
+      transform: scale(1.05);
+    }
   }
 `
 
@@ -20,10 +27,12 @@ const GatsbyImage = ({ index, onClick, photo, margin }) => (
     style={{ margin, height: photo.height, width: photo.width }}
     onClick={e => onClick(e, { index, photo })}
   >
-    <Img
-      fixed={typeof window === 'undefined' ? { src: {} } : undefined}
-      fluid={photo.fluid}
-    />
+    <div>
+      <Img
+        fixed={typeof window === 'undefined' ? { src: {} } : undefined}
+        fluid={photo.fluid}
+      />
+    </div>
   </ImageWrapper>
 )
 
