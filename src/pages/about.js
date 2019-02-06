f3bbdb5156d0c08e207fc881c318ab3e1875ce26
Will Adams <bushblade@gmail.com>
2019-02-06 13:00:25 +0000

    text

--- src/pages/about.js
+++ src/pages/about.js
@@ -185,7 +185,8 @@ const IndexPage = () => (
             here.
           </a>{' '}
           For those geeky enough to want to know - this site is built with
-          GatsbyJS, ReactJS and styled-components with continuous deployment to
+          GatsbyJS, ReactJS and styled-components mainly using VS Code on a
+          desktop instalation of Manjaro Linux with continuous deployment to
           Netlify through GitHub.
         </p>
       </Layout>
