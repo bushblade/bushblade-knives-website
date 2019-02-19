be89e92700fd5a0735886d7e87d0cc958b63b76a
Will Adams <bushblade@gmail.com>
2019-02-19 11:43:54 +0000

    svg animation logic in separate render props component

---
+++ src/components/pathAnimation.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { useSpring, config } from 'react-spring'
+import Observer from '@researchgate/react-intersection-observer'
+
+const PathAnimation = ({ children }) => {
+  const [inview, set] = useState(false)
+
+  const pathSpring = useSpring({
+    opacity: inview ? 1 : 0,
+    strokeDashoffset: inview ? 0 : 100,
+    from: { strokeDashoffset: 100, opacity: 0, strokeDasharray: 100 },
+    config: config.molasses,
+    delay: 200,
+  })
+
+  const options = {
+    onChange: event => (event.isIntersecting ? set(true) : null),
+    threshold: 1,
+    rootMargin: '0% 0% 0%',
+  }
+
+  return <Observer {...options}>{children(pathSpring)}</Observer>
+}
+export default PathAnimation
--- src/pages/work/woodloreSpineSVG.js
+++ src/pages/work/woodloreSpineSVG.js
@@ -1,73 +1,60 @@
-import React, { useState } from 'react'
-import { useSpring, animated, config } from 'react-spring'
-import Observer from '@researchgate/react-intersection-observer'
+import React from 'react'
+import { animated } from 'react-spring'
+
+import PathAnimation from '../../components/pathAnimation'
 
 const SVG = () => {
-  const [inview, set] = useState(false)
-
-  const pathSpring = useSpring({
-    opacity: inview ? 1 : 0,
-    strokeDashoffset: inview ? 0 : 100,
-    from: { strokeDashoffset: 100, opacity: 0 },
-    config: config.molasses,
-    delay: 200,
-  })
-
-  const options = {
-    onChange: event => (event.isIntersecting ? set(true) : null),
-    threshold: 1,
-    rootMargin: '0% 0% 0%',
-  }
-
   return (
-    <Observer {...options}>
-      <animated.svg
-        xmlns="http://www.w3.org/2000/svg"
-        id="svg842"
-        viewBox="0 0 208.79437 23.714577"
-        style={{ strokeDasharray: 100, ...pathSpring }}
-      >
-        <g transform="translate(-33.004 -177.295)" id="layer1">
-          <g id="g1429" stroke="#333">
-            <g id="g1460">
-              <g id="g1447" fill="none" strokeWidth="0.529">
-                <path
-                  pathLength="100"
-                  d="m 33.270244,188.15222 103.354146,-1 h 97.8378 l 7.26598,2"
-                  id="path1413"
-                />
-                <path
-                  pathLength="100"
-                  d="m 33.270245,187.15222 v -8.25194 c -0.03477,-0.99417 0.470871,-1.34234 1.107298,-1.34076 29.93419,5.36127 40.50273,-1.97605 96.874687,2.17336 l 5.37216,7.41934"
-                  id="path1425"
-                />
+    <PathAnimation>
+      {styles => (
+        <animated.svg
+          xmlns="http://www.w3.org/2000/svg"
+          id="svg842"
+          viewBox="0 0 208.79437 23.714577"
+          style={styles}
+        >
+          <g transform="translate(-33.004 -177.295)" id="layer1">
+            <g id="g1429" stroke="#333">
+              <g id="g1460">
+                <g id="g1447" fill="none" strokeWidth="0.529">
+                  <path
+                    pathLength="100"
+                    d="m 33.270244,188.15222 103.354146,-1 h 97.8378 l 7.26598,2"
+                    id="path1413"
+                  />
+                  <path
+                    pathLength="100"
+                    d="m 33.270245,187.15222 v -8.25194 c -0.03477,-0.99417 0.470871,-1.34234 1.107298,-1.34076 29.93419,5.36127 40.50273,-1.97605 96.874687,2.17336 l 5.37216,7.41934"
+                    id="path1425"
+                  />
+                </g>
               </g>
-            </g>
-            <g transform="matrix(1 0 0 -1 0 378.304)" id="g1468">
-              <g id="g1466" fill="none" strokeWidth="0.529">
-                <path
-                  pathLength="100"
-                  id="path1462"
-                  d="m 33.270244,188.15222 103.354146,-1 h 97.8378 l 7.26598,2"
-                />
-                <path
-                  pathLength="100"
-                  id="path1464"
-                  d="m 33.270245,187.15222 v -8.25194 c -0.03477,-0.99417 0.470871,-1.34234 1.107298,-1.34076 29.93419,5.36127 40.50273,-1.97605 96.874687,2.17336 l 5.37216,7.41934"
-                />
+              <g transform="matrix(1 0 0 -1 0 378.304)" id="g1468">
+                <g id="g1466" fill="none" strokeWidth="0.529">
+                  <path
+                    pathLength="100"
+                    id="path1462"
+                    d="m 33.270244,188.15222 103.354146,-1 h 97.8378 l 7.26598,2"
+                  />
+                  <path
+                    pathLength="100"
+                    id="path1464"
+                    d="m 33.270245,187.15222 v -8.25194 c -0.03477,-0.99417 0.470871,-1.34234 1.107298,-1.34076 29.93419,5.36127 40.50273,-1.97605 96.874687,2.17336 l 5.37216,7.41934"
+                  />
+                </g>
               </g>
+              <path
+                pathLength="100"
+                id="path1470"
+                d="m 33.270245,187.15222 v 4"
+                fill="none"
+                strokeWidth="0.529"
+              />
             </g>
-            <path
-              pathLength="100"
-              id="path1470"
-              d="m 33.270245,187.15222 v 4"
-              fill="none"
-              strokeWidth="0.529"
-            />
           </g>
-        </g>
-      </animated.svg>
-    </Observer>
+        </animated.svg>
+      )}
+    </PathAnimation>
   )
 }
 export default SVG
