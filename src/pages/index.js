0d2c82caee3a8ca56c46d94ff6ae495eaae66d5c
Will Adams <bushblade@gmail.com>
2019-06-06 18:19:59 +0100

    useStaticQuery hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StaticQuery, Link, graphql } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
 import Img from 'gatsby-image'
@@ -48,93 +48,88 @@ const indexQuery = graphql`
   }
 `
 
-const IndexPage = ({ location }) => (
-  <StaticQuery
-    query={indexQuery}
-    render={data => (
-      <Layout
-        banner={data.banner.childImageSharp.fluid}
-        pageTitle="Welcome to Bushblade"
-        tagline={' handmade knives by Will Adams'}
-        keywords={[
-          'ray mears',
-          'alan wood',
-          'bushcraft',
-          'handmade',
-          'knife',
-          'uk',
-        ]}
-        location={location}
-      >
-        <ReverseContainer narrow>
-          <ReverseCell area="right">
-            <Img
-              fluid={data.profile.childImageSharp.fluid}
-              title="Will Adams"
+const IndexPage = ({ location }) => {
+  const data = useStaticQuery(indexQuery)
+  return (
+    <Layout
+      banner={data.banner.childImageSharp.fluid}
+      pageTitle="Welcome to Bushblade"
+      tagline={' handmade knives by Will Adams'}
+      keywords={[
+        'ray mears',
+        'alan wood',
+        'bushcraft',
+        'handmade',
+        'knife',
+        'uk',
+      ]}
+      location={location}
+    >
+      <ReverseContainer narrow>
+        <ReverseCell area="right">
+          <Img fluid={data.profile.childImageSharp.fluid} title="Will Adams" />
+          <br />
+        </ReverseCell>
+        <ReverseCell area="left">
+          <blockquote style={{ maxWidth: '800px', margin: 'auto' }}>
+            <FontAwesomeIcon
+              icon={faQuoteLeft}
+              size="xs"
+              style={{ marginBottom: '0.3rem', maxWidth: '1.5rem' }}
+            />{' '}
+            Having used and acquired many knives over the years, from a variety
+            of brand name manufacturers and without sufficient funds to purchase
+            my ideal knife I decided to try and make my own.{' '}
+            <FontAwesomeIcon
+              icon={faQuoteRight}
+              size="xs"
+              style={{ marginBottom: '0.3rem', maxWidth: '1.5rem' }}
             />
-            <br />
-          </ReverseCell>
-          <ReverseCell area="left">
-            <blockquote style={{ maxWidth: '800px', margin: 'auto' }}>
-              <FontAwesomeIcon
-                icon={faQuoteLeft}
-                size="xs"
-                style={{ marginBottom: '0.3rem', maxWidth: '1.5rem' }}
-              />{' '}
-              Having used and acquired many knives over the years, from a
-              variety of brand name manufacturers and without sufficient funds
-              to purchase my ideal knife I decided to try and make my own.{' '}
-              <FontAwesomeIcon
-                icon={faQuoteRight}
-                size="xs"
-                style={{ marginBottom: '0.3rem', maxWidth: '1.5rem' }}
-              />
-            </blockquote>
-            <br />
-            <p>
-              <strong>I made my first knives in 2001</strong> yielding
-              functional success in replicating the now famous Woodlore knives
-              designed by Ray Mears and at the time made by Alan Wood. I still
-              own these knives but the quality of my work has improved somewhat
-              since the early days.
-            </p>
-            <p>
-              <strong>I am a firm believer in form following function.</strong>{' '}
-              I aim to make a functional tool that will be a pleasure to own and
-              use for many years. The overall fit, finish and quality of work
-              are of the utmost importance to me when making a knife. Each knife
-              and sheath is handmade by me here in the UK, I do not currently
-              outsource any part of the process.
-            </p>
-          </ReverseCell>
-        </ReverseContainer>
-
-        <NarrowContainer style={{ textAlign: 'center' }}>
+          </blockquote>
+          <br />
           <p>
-            I only spend a few days each week making knives as the majority of
-            my time is dedicated to my twin girls. Please take a look around,
-            and feel free to <Link to="/contact">contact me</Link> with any
-            comments or enquiries.
+            <strong>I made my first knives in 2001</strong> yielding functional
+            success in replicating the now famous Woodlore knives designed by
+            Ray Mears and at the time made by Alan Wood. I still own these
+            knives but the quality of my work has improved somewhat since the
+            early days.
           </p>
-          <h2 style={{ color: '#a94442' }}>
-            You must be over 18 years of age to order a knife.
-          </h2>
-        </NarrowContainer>
-        <Gallery
-          columns={width => {
-            if (width < 700) {
-              return 2
-            } else if (width < 1000) {
-              return 3
-            } else {
-              return 6
-            }
-          }}
-          photos={data.allFile.edges}
-        />
-      </Layout>
-    )}
-  />
-)
+          <p>
+            <strong>I am a firm believer in form following function.</strong> I
+            aim to make a functional tool that will be a pleasure to own and use
+            for many years. The overall fit, finish and quality of work are of
+            the utmost importance to me when making a knife. Each knife and
+            sheath is handmade by me here in the UK, I do not currently
+            outsource any part of the process.
+          </p>
+        </ReverseCell>
+      </ReverseContainer>
+
+      <NarrowContainer style={{ textAlign: 'center' }}>
+        <p>
+          I only spend a few days each week making knives as the majority of my
+          time is dedicated to my twin girls. Please take a look around, and
+          feel free to <Link to="/contact">contact me</Link> with any comments
+          or enquiries.
+        </p>
+        <h2 style={{ color: '#a94442' }}>
+          You must be over 18 years of age to order a knife.
+        </h2>
+      </NarrowContainer>
+      <Gallery
+        columns={width => {
+          if (width < 700) {
+            return 2
+          } else if (width < 1000) {
+            return 3
+          } else {
+            return 6
+          }
+        }}
+        photos={data.allFile.edges}
+      />
+    </Layout>
+  )
+}
 
 export default IndexPage
