c8d5a891695d5a1f69f14c2e02a5b37e0b1d96d3
Will Adams <bushblade@gmail.com>
2019-09-09 11:29:53 +0100

    refactor

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -2,17 +2,12 @@ import { useState, useEffect } from 'react'
 
 export const useIsMobile = () => {
   const isMobile = () => {
-    if (typeof window !== 'undefined') {
-      return window.innerWidth < 780
-    }
+    if (typeof window !== 'undefined') return window.innerWidth < 780
   }
   const [mobile, setMobile] = useState(isMobile())
 
-  const checkWindowSize = () => {
-    isMobile() ? setMobile(true) : setMobile(false)
-  }
-
   useEffect(() => {
+    const checkWindowSize = () => setMobile(isMobile())
     if (window) {
       checkWindowSize()
       window.addEventListener('resize', checkWindowSize)
