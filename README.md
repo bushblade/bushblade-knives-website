213e79fafab6ac4e87b12fd7464330d3337910c2
Will Adams <bushblade@gmail.com>
2020-11-03 17:46:53 +0000

    basics of react-spring-slider working

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 [bushblade](https://bushblade.co.uk/) handmade knives by Will Adams
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/0dda1f0d-369d-46e4-9afe-0d07fe8ec301/deploy-status)](https://app.netlify.com/sites/bushblade-knives/deploys)
+
+# TODO
+- replace react-images with a react-sring carousel ✅
+- close modal button
+- modal keydown listeners
+- prevent body scroll when modal open
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,32 @@
 import React, { useState } from 'react'
 import Gallery from 'react-photo-gallery'
-import Carousel, { Modal, ModalGateway } from 'react-images'
+// import Carousel from 'react-images'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
+import Slider from '@farbenmeer/react-spring-slider'
+
+const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  background: rgba(0, 0, 0, 0.8);
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  padding: 0;
+  margin: 0;
+`
+const SliderImageWrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+`
 
 const ImageWrapper = styled.div`
   box-shadow: -1px 3px 6px 1px rgba(0, 0, 0, 0.3);
@@ -24,20 +48,21 @@ const ImageWrapper = styled.div`
 const GatsbyImage = ({ index, onClick, photo, margin }) => (
   <ImageWrapper
     style={{ margin, height: photo.height, width: photo.width }}
-    onClick={e => onClick(e, { index, photo })}
+    onClick={(e) => onClick(e, { index, photo })}
     key={photo.key}
   >
     <Img
+      style={{ maxWidth: '100vw', maxHeight: '100vh' }}
       fixed={typeof window === 'undefined' ? { src: {} } : undefined}
       fluid={photo.fluid}
     />
   </ImageWrapper>
 )
 
-const fileNumber = file =>
+const fileNumber = (file) =>
   Number(file.node.childImageSharp.fluid.originalName.replace(/[a-z]/gi, ''))
 
-const getImages = imageArray => {
+const getImages = (imageArray) => {
   return [...imageArray]
     .sort((a, b) => fileNumber(b) - fileNumber(a))
     .map(({ node: { childImageSharp: { fluid, original } } }) => ({
@@ -50,7 +75,7 @@ const getImages = imageArray => {
     }))
 }
 
-const styleFn = styleObj => ({ ...styleObj, zIndex: 100 })
+const styleFn = (styleObj) => ({ ...styleObj, zIndex: 100 })
 
 const KnifeGallery = ({ photos, ...rest }) => {
   const [isOpen, setOpen] = useState(false)
@@ -75,19 +100,25 @@ const KnifeGallery = ({ photos, ...rest }) => {
         />
       )}
 
-      <ModalGateway>
-        {isOpen ? (
-          <Modal
-            onClose={() => {
-              setCurrent(0)
-              setOpen(false)
-            }}
-            styles={{ blanket: styleFn, positioner: styleFn }}
-          >
-            <Carousel views={images} currentIndex={current} />
-          </Modal>
-        ) : null}
-      </ModalGateway>
+      {isOpen ? (
+        <Modal>
+          <Slider hasArrows hasBullets activeIndex={current}>
+            {images.map((image) => (
+              <div
+                key={image.key}
+                style={{
+                  backgroundSize: 'contain',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundImage: `url(${image.src})`,
+                  height: '100%',
+                  width: '100%',
+                }}
+              ></div>
+            ))}
+          </Slider>
+        </Modal>
+      ) : null}
     </div>
   )
 }
